50060cf30500e51b224592801d6c68fbff600b9d
kgrechin <kgrechin@users.noreply.github.com>
2022-10-23 18:46:59 +0000

    deploy: d316d30a0d9baf1d9d2145ed2f802def04b4f9f8

--- js/index.bundle.js
+++ js/index.bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("window.location.href = '../chats.html';\n\n//# sourceURL=webpack:///./src/js/index.js?");
+eval("window.location.href = './chats.html';\n\n//# sourceURL=webpack:///./src/js/index.js?");
 
 /***/ })
 
